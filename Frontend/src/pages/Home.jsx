0ed2bf23946c84b7cc00b2926e8af4355291a713
saugat dhungana <riggerbenu.123@gmail.com>
2025-06-08 19:50:53 +0545

    login signup pages

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = ({ user }) => (
   <div className="text-center">
     <h1 className="text-2xl font-bold mb-4">Welcome, {user.email}!</h1>
-    <p>You are logged in as <b>{user.role}</b>.</p>
+    <p>You are logged in as  <b>{user.role}</b>.</p>
   </div>
 );
 

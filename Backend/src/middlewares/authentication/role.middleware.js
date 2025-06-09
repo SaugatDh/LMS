87765193331eaa7482f0d4e463745ef5db90110a
AnilHasda <hasdaanil098@gmail.com>
2025-06-09 14:01:35 +0545

    add admin controller,route and related files

--- Backend/src/middlewares/authentication/role.middleware.js
+++ Backend/src/middlewares/authentication/role.middleware.js
@@ -2,8 +2,12 @@
 const isAuthenticated=(roles)=>{
   return (req,res,next)=>{
   const {role}=req.loggedInfo;
+  console.log({role});
   if(!role) return res.status(401).json({statusCode:401,message:"please login to access this service !"});
-    if(roles.includes(role)) next();
+    if(roles.includes(role)) {
+      console.log("user is authenticated");
+      return next();
+    }
      return res.status(401).json({statusCode:401,message:"sorry! you are not authenticate to access this service !"});
  
   }
